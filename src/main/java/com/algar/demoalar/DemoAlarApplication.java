4d7ee07e5aefd0541f2f417493821eaf339c16e2
CarlosBotelho42 <carlospiece55@gmail.com>
2021-03-02 16:37:24 -0300

    Opa

---
+++ src/main/java/com/algar/demoalar/DemoAlarApplication.java
@@ -0,0 +1,13 @@
+package com.algar.demoalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAlarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAlarApplication.class, args);
+	}
+
+}
